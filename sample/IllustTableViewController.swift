58981033e6d4b63b954b6d8270aa6a453d229d22
chocomelon <chocomelon@pixiv.com>
2017-04-10 09:16:05 +0900

    IllustTableViewControllerのレイアウト作成

---
+++ sample/IllustTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IllustTableViewController.swift
+//  sample
+//
+//  Created by MASAKI MITSUYAMA on 2017/04/10.
+//  Copyright © 2017 pixiv Inc. All rights reserved.
+//
+
+import UIKit
+
+class IllustTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
